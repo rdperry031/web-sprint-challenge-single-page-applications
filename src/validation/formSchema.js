6f0d3ab9a17bd56b3dadbd6790eb754ad7097c2b
rdperry031 <rdperry031@gmail.com>
2021-08-20 12:57:35 -0500

    completed formSchema

--- src/validation/formSchema.js
+++ src/validation/formSchema.js
@@ -1,7 +1,29 @@
 import * as yup from 'yup'
 
 const formSchema = yup.object().shape({
-    name:yup
+    name: yup
+        .string()
+        .trim()
+        .required('Name is required'),
+    size: yup
+        .string()
+        .trim()
+        .required('Size is required'),
+    special: yup
+        .string() 
+        .trim(),
+    
+    pepperoni: yup.boolean(),
+    gabagool: yup.boolean(),
+    grilledChicken: yup.boolean(),
+    canadianBacon: yup.boolean(),    
+    bacon: yup.boolean(),
+    onion: yup.boolean(),
+    greenPepper: yup.boolean(),
+    jalapenos: yup.boolean(),
+    pineapple: yup.boolean(),
+    olives: yup.boolean(),
+    mushrooms: yup.boolean(),
 })
 
 export default formSchema
